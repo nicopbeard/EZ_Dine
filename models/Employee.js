07ded0185fc1dc208e0837b49ad37e605615f960
npb221 <npb221@lehigh.edu>
2020-11-23 14:10:58 -0500

    working on api's

--- models/Employee.js
+++ models/Employee.js
@@ -5,12 +5,6 @@ const employeeSchema = new mongoose.Schema({
     username : {type : String, unique : true},
     password : {type : String, required : true},
     date : Date,
-    FilmReviews : [{
-        review_id : Number,
-        title : String,
-        body : String,
-        date : Date
-    }]
  });
 
 
