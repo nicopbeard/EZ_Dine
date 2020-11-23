f7f7d54adfb28d2109603e8cf6dc50370614781e
npb221 <npb221@lehigh.edu>
2020-11-23 16:32:41 -0500

    working on apis

--- models/Employee.js
+++ models/Employee.js
@@ -4,9 +4,8 @@ const Schema = mongoose.Schema;
 const employeeSchema = new mongoose.Schema({
     username : {type : String, unique : true},
     password : {type : String, required : true},
-    date : Date,
+    date : Date
  });
-
 
 const Employee = mongoose.model('Employee', employeeSchema);
 
