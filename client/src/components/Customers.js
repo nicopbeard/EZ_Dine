268e4b2edc2be51726a5fd6d51bbb436875edf82
Elliot Scribner <ejscribner@gmail.com>
2020-11-26 20:55:10 -0700

    update customers bug

--- client/src/components/Customers.js
+++ client/src/components/Customers.js
@@ -5,12 +5,16 @@ class Customers extends Component {
   constructor() {
     super();
     this.state = {
-      customers: []
+      customers: [],
+      employees: []
     };
   }
 
   componentDidMount() {
-    fetch('/api/customers')
+    fetch('/employees')
+        .then(res => res.json())
+        .then(employees => this.setState({employees}, () => console.log('Employees fetched...', employees)));
+    fetch('/customers')
         .then(res => res.json())
         .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
   }
@@ -18,10 +22,16 @@ class Customers extends Component {
   render() {
     return (
         <div>
+          <h2>Employees</h2>
+          <ul>
+            {this.state.employees.map(employee =>
+                <li key={employee._id}> User:{employee.username} PW:{employee.password} </li>
+            )}
+          </ul>
           <h2>Customers</h2>
           <ul>
             {this.state.customers.map(customer =>
-                <li key={customer._id}> User:{customer.username} PW:{customer.password}</li>
+                <li key={customer._id}> User:{customer.username} PW:{customer.password} </li>
             )}
           </ul>
         </div>
